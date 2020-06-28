acc8d4d645d7a96ff28c68723aceb48b30691795
harikudil <mshriharish@gmail.com>
2020-06-29 04:49:01 +0800

    2 layer and 3 layer completed

--- NN_3_layers.py
+++ NN_3_layers.py
@@ -7,6 +7,7 @@ from sklearn.decomposition import PCA
 from glob import iglob
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import plot_confusion_matrix
 import itertools
 
 def preprocess(dataset):
@@ -35,9 +36,9 @@ def preprocess(dataset):
             fruits_output.append(1)
         if label == 'Pear':
             fruits_output.append(2)
-        plt.figure(0)
+        
         img = imread(path)
-        #plt.imshow(img[:,:,0])
+       
     
         fruit = pd.Series(img[:,:,0].flatten(),name = label)
         fruits = fruits.append(fruit)
@@ -66,8 +67,8 @@ def preprocess(dataset):
     
     fruits_pca = PCA(n_components=2)
     fruits_input = fruits_pca.fit_transform(scaled_fruits)
-    
     #plt.scatter(fruits_input[:,0],fruits_input[:,1],c=label_clr)
+    
     return fruits_input,one_hot_fruits_output
 
 class dlnet:
@@ -76,7 +77,7 @@ class dlnet:
         self.Y=y
         self.Yh=np.zeros((1,self.Y.shape[0]))
         self.L=2
-        self.dims = [2, 5, 3]
+        self.dims = [2, 4, 4, 3]
         self.param = {}
         self.ch = {}
         self.grad = {}
@@ -86,10 +87,12 @@ class dlnet:
         
 def nInit(self):    
         np.random.seed(1)
-        self.param['W1'] = np.random.randn(self.dims[1], self.dims[0]).T / np.sqrt(self.dims[0]) 
-        self.param['b1'] = np.zeros((self.dims[1]))        
-        self.param['W2'] = np.random.randn(self.dims[2], self.dims[1]).T / np.sqrt(self.dims[1]) 
-        self.param['b2'] = np.zeros((self.dims[2]))                
+        self.param['Wh1'] = np.random.randn(self.dims[1], self.dims[0]).T / np.sqrt(self.dims[0]) 
+        self.param['bh1'] = np.zeros((self.dims[1]))        
+        self.param['Wh2'] = np.random.randn(self.dims[2], self.dims[1]).T / np.sqrt(self.dims[1]) 
+        self.param['bh2'] = np.zeros((self.dims[2]))     
+        self.param['Wo'] = np.random.randn(self.dims[3], self.dims[2]).T / np.sqrt(self.dims[2]) 
+        self.param['bo'] = np.zeros((self.dims[3]))           
         return
     
 def Sigmoid(Z):
@@ -104,15 +107,20 @@ def Softmax(A):
 
 def forward(self):    
     
-    Z1 = np.dot(self.X,self.param['W1'])+self.param['b1']
-    A1 = Sigmoid(Z1)
-    self.ch['Z1'],self.ch['A1']=Z1,A1
+    Zh1 = np.dot(self.X,self.param['Wh1'])+self.param['bh1']
+    Ah1 = Sigmoid(Zh1)
+    self.ch['Zh1'],self.ch['Ah1']=Zh1,Ah1
     
-    Z2 = np.dot(A1,self.param['W2']) + self.param['b2'] 
-    A2 = Softmax(Z2)
-    self.ch['Z2'],self.ch['A2']=Z2,A2
-    self.Yh=A2
-    loss=nloss(self,A2)
+    Zh2 = np.dot(Ah1,self.param['Wh2']) + self.param['bh2'] 
+    Ah2 = Softmax(Zh2)
+    self.ch['Zh2'],self.ch['Ah2']=Zh2,Ah2
+    
+    Zo = np.dot(Ah2,self.param['Wo']) + self.param['bo'] 
+    Ao = Softmax(Zo)
+    self.ch['Zo'],self.ch['Ao']=Zh2,Ah2
+    
+    self.Yh=Ao
+    loss=nloss(self,Ao)
     return self.Yh, loss
 
 def nloss(self,Yh):
@@ -123,25 +131,41 @@ def nloss(self,Yh):
     
 
 def backward(self):
-    dcost_dz2 = self.Yh - self.Y
-    dz2_dw2 = self.ch['A1']
-    dcost_w2 = np.dot(dz2_dw2.T, dcost_dz2)
-    dcost_b2 = dcost_dz2
+    dcost_dzo = self.Yh - self.Y
+    dzo_dwo = self.ch['Ah2']
+    dcost_wo = np.dot(dzo_dwo.T, dcost_dzo)
+    dcost_bo = dcost_dzo
 
-    ########## Phases 2
+########## Phases 2
 
-    dz2_da1 = self.param['W2']
-    dcost_da1 = np.dot(dcost_dz2 , dz2_da1.T)
-    da1_dz1 = dSigmoid(self.ch['Z1'])
-    dz1_dw1 = self.X
-    dcost_w1 = np.dot(dz1_dw1.T, da1_dz1 * dcost_da1)
-    dcost_b1 = dcost_da1 * da1_dz1
+    dzo_dah2 = self.param['Wo']
+    dcost_dah2 = np.dot(dcost_dzo , dzo_dah2.T)
+    dah2_dzh2 = dSigmoid(self.ch['Zh2'])
+    dzh2_dwh2 = self.ch['Ah1']
+    dcost_wh2 = np.dot(dzh2_dwh2.T, dah2_dzh2 * dcost_dah2)
+    dcost_bh2 = dcost_dah2 * dah2_dzh2
+    
+    ########## Phases 3
 
+    #dzo_dah2 = self.param['Wo']
+    #dcost_dah2 = np.dot(dcost_dzo , dzo_dah2.T)
+    #dah2_dzh2 = dSigmoid(self.ch['Zh2'])
+    dzh2_dah1 = self.param['Wh2']
+    dah1_dzh1 = dSigmoid(self.ch['Zh1'])
+    dzh1_dwh1 = self.X
+#    dzh2_dwh1 = np.dot(dzh1_dwh1.T,np.dot(dzh2_dah1,dah1_dzh1.T).T)
+   # dcost_dzh2=  dah2_dzh2 * dcost_dah2
+    #dcost_dwh1 = np.dot(dzh2_dwh1,dcost_dzh2.T)
+    dcost_bh1 = np.dot((dah2_dzh2 * dcost_dah2).T, np.dot(dzh2_dah1,dah1_dzh1.T).T)
+    dcost_dwh1 = np.dot(((dah2_dzh2 * dcost_dah2).T * np.dot(dzh2_dah1,dah1_dzh1.T)),dzh1_dwh1).T
+    
     # Update Weights ================
-    self.param["W1"] -= self.lr * dcost_w1
-    self.param["b1"] -= self.lr * dcost_b1.sum(axis=0)  
-    self.param["W2"] -= self.lr * dcost_w2   
-    self.param["b2"] -= self.lr * dcost_b2.sum(axis=0)  
+    self.param["Wh1"] -= self.lr * dcost_dwh1
+    self.param["bh1"] -= self.lr * dcost_bh1.sum(axis=0)  
+    self.param["Wh2"] -= self.lr * dcost_wh2   
+    self.param["bh2"] -= self.lr * dcost_bh2.sum(axis=0)  
+    self.param["Wo"] -= self.lr * dcost_wo   
+    self.param["bo"] -= self.lr * dcost_bo.sum(axis=0)  
     
     ''' dLoss_Yh = - (np.divide(self.Y.T, self.Yh ) - np.divide(1 - self.Y.T, 1 - self.Yh))    
         
@@ -188,9 +212,9 @@ def pred(self,x, y):
         self.X=x
         self.Y=y
         pred, loss= forward(self)    
+        comm = np.zeros((y.shape))
         print("\n\nPREDICTED OUTPUT")
         print(pred)
-        comm = np.zeros((y.shape))
         pred_val = np.zeros((y.shape[0]))
         exp_output = np.zeros((y.shape[0]))
         for i in range(0, pred.shape[0]):
@@ -203,6 +227,7 @@ def pred(self,x, y):
         return pred,comm,pred_val,exp_output
     
 def plot_confusion(expected,predicted):
+    plt.figure(0)
     cf =confusion_matrix(expected,predicted)
     plt.imshow(cf,cmap=plt.cm.Blues,interpolation='nearest')
     plt.colorbar()
